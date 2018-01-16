1aec053cc473e0f20dd24367bf9721f5234820a8
migvasc <miguel.vasconcelos@usp.br>
2018-01-16 02:56:05 +0000

    teste travis 2

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -28,18 +28,7 @@ RSpec.describe ProjectsController, type: :controller do
   # This should return the minimal set of attributes required to create a valid
   # Project. As you add validations to Project, be sure to
   # adjust the attributes here as well.
-  let(:valid_attributes) {
-    skip("Add a hash of attributes valid for your model")
-  }
-
-  let(:invalid_attributes) {
-    skip("Add a hash of attributes invalid for your model")
-  }
-
-  # This should return the minimal set of values that should be in the session
-  # in order to pass any filters (e.g. authentication) defined in
-  # ProjectsController. Be sure to keep this updated too.
-  let(:valid_session) { {} }
+  
 
   describe "GET #index" do
     it "returns a success response" do
